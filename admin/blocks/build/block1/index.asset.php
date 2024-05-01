2e3bb3f8eb0e2717e812f19c21d9c6c6ceb8bdb1
Wilhem Arnoldy <contact@wilhemarnoldy.fr>
2024-05-01 16:02:07 +0200

    Update blocks

--- admin/blocks/build/block1/index.asset.php
+++ admin/blocks/build/block1/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-core-data', 'wp-element', 'wp-i18n'), 'version' => '4fc73393469c87128cc7');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-core-data', 'wp-element', 'wp-i18n'), 'version' => '0c6a7f46829cedd18f86');
--- admin/blocks/build/block1/render.php
+++ admin/blocks/build/block1/render.php
@@ -956,7 +956,7 @@ foreach($terms_list as $terms_name) {
 						<picture class="lazy" data-fancybox="gallery" data-loader="pic" data-src="<?= $image['full_url'] ?>">
 							<data-src media="(min-width: 150px)" srcset="<?= $image['sizes']['large']['url']; ?>" type="image/jpeg"></data-src>
 							<data-src media="(min-width: 150px)" srcset="<?= $image['sizes']['medium']['url']; ?>" type="image/jpeg"></data-src>
-							<data-img src="<?= $image['url']; ?>" alt="<?= esc_html($image['alt']); ?>" class="img-fluid --rounded-4 rounded-top-4 rounded-top-left-0 --shadow-lg h-300-px fit-image w-100" style="" title="<?= $image['title']; ?>"></data-img>
+							<data-img src="<?= $image['url']; ?>" alt="<?= esc_html($image['alt']); ?>" class="img-fluid --rounded-4 rounded-top-4 rounded-top-left-0 --shadow-lg h-300-px fit-image w-100 bg-light" style="" title="<?= $image['title']; ?>"></data-img>
 						</picture>
 						<?php if ( $image['alt'] || $image['description'] ) : ?>
 						<figcaption><strong>© <?= esc_html($image['alt']); ?></strong> <?= esc_html($image['description']); ?></figcaption>
@@ -969,7 +969,7 @@ foreach($terms_list as $terms_name) {
 
 			<!-- Featured image -->  
 			<?php if (!empty($o_featured_img_urls)): ?>
-				<div class="row row-cols-sm-2 row-cols-lg-3 mt-2 mb-6 g-4">
+				<div class="row row-cols-sm-2 row-cols-lg-3 mt-2 --mb-6 g-4">
 					<a class="col " href="javascript:;">
 						<figure title="<?php echo esc_attr($o_featured_img_description); ?>">
 							<picture class="--contrast--light overflow-hidden h-100 lazy" data-fancybox="logotypes" data-loader="pic" data-src="<?= $o_featured_img_url_full ?>" data-aos="fade-up" data-aos-delay="200">
@@ -1082,7 +1082,7 @@ foreach($terms_list as $terms_name) {
 										srcset="<?= $s_featured_img_urls['post-featured-image-m']; ?>" type="image/jpeg"></data-src>
 								<data-src media="(min-width: 380px)"
 										srcset="<?= $s_featured_img_urls['post-featured-image-s']; ?>" type="image/jpeg"></data-src>
-								<data-img src="<?= $s_featured_img_urls['thumbnail']; ?>" alt="<?= esc_html($s_featured_img_caption); ?>" class="img-fluid rounded-4 shadow-lg --h-300-px fit-image w-100 img-transition-scale"></data-img>
+								<data-img src="<?= $s_featured_img_urls['thumbnail']; ?>" alt="<?= esc_html($s_featured_img_caption); ?>" class="img-fluid rounded-4 shadow-lg --h-300-px fit-image w-100 img-transition-scale bg-light"></data-img>
 								</picture>
 								<?php if ( $s_featured_img_caption || $s_featured_img_description ) : ?>
 								<figcaption><strong>© <?= esc_html($s_featured_img_caption); ?></strong> <?= esc_html($s_featured_img_description); ?></figcaption>
