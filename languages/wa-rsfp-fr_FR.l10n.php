f6cb06488d0d03e27b9380f412a590521e93b32e
Wilhem Arnoldy <contact@wilhemarnoldy.fr>
2024-06-12 12:34:47 +0200

    Start i18n

---
+++ languages/wa-rsfp-fr_FR.l10n.php
@@ -0,0 +1,2 @@
+<?php
+return ['project-id-version'=>'WA RSFP Global functions','report-msgid-bugs-to'=>'','pot-creation-date'=>'2024-06-12 10:31+0000','po-revision-date'=>'2024-06-12 10:33+0000','last-translator'=>'','language-team'=>'Français','language'=>'fr_FR','plural-forms'=>'nplurals=2; plural=n > 1;','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','x-generator'=>'Loco https://localise.biz/','x-loco-version'=>'2.6.9; wp-6.5.4','x-domain'=>'wa-rsfp','messages'=>[]];
